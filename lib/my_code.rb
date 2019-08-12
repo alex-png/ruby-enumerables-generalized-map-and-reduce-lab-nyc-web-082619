cb6d813522209e9b642f1e35a9327dedb940f037
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-12 18:51:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,10 @@ end
 
 
 def reduce(source_array, starting_point = 0)
-  new = starting_point
-  new += yield(source_array)
-  new
+new = starting_point
+count = 0
+while count < source_array.size 
+new << yield(source_array[count])
+count += 1 
 end
+new
