47059b56fc9db2ac4f0e7b916d89f3f27e79df10
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-12 19:17:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,11 +15,14 @@ def reduce(source_array, starting_point = 0)
 source_array << starting_point
 sum = 0
 count = 0
-  if source_array[count]
-    sum += yield(source_array[count])
-    count += 1
-    elsif source_array[count] == false 
-      return false
-  end
+  while count < source_array.size
+    if source_array[count] == false 
+      source_array = false 
+    elsif source_array[count] == true 
+      source_array = true 
+    else sum << yield(source_array[count])
+    end
+    count += 1 
+  end 
   sum
-end
+end 
