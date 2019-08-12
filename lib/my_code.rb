ceae03bbd57afbae4ee18562c29eebaa3663dcfb
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-12 19:01:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,4 +10,36 @@ new
 end
 
 
+
 def reduce(source_array, starting_point = 0)
+source_array << starting_point
+sum = 0
+count = 0
+  while source_array[count]
+    sum += source_array[count]
+    count += 1
+  end
+  sum
+end
+
+def reduce_to_all_true(source_array)
+   i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == false)
+      return false
+    end
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+   i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == true)
+      return true
+    end
+    i += 1
+  end
+  return false
+end
