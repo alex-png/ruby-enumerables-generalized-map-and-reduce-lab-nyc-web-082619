c592c69f7645f5e1daba97e164356cfc917f7703
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-12 19:08:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,24 +22,4 @@ count = 0
   sum
 end
 
-def reduce_to_all_true(source_array)
-   i = 0 
-  while i < source_array.length do 
-    if (source_array[i] == false)
-      return false
-    end
-    i += 1
-  end
-  return true
-end
 
-def reduce_to_any_true(source_array)
-   i = 0 
-  while i < source_array.length do 
-    if (source_array[i] == true)
-      return true
-    end
-    i += 1
-  end
-  return false
-end
